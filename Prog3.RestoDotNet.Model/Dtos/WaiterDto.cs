8f37c375131c2660d03e4b1d48f7fafb778c3719
Leonardo <leitee23@hotmail.com>
2019-06-23 21:49:11 -0300

    matching projects namespaces

---
+++ Prog3.RestoDotNet.Model/Dtos/WaiterDto.cs
@@ -0,0 +1,12 @@
+﻿using Prog3.RestoDotNet.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prog3.RestoDotNet.Model.Dtos
+{
+    public class WaiterDto : Waiter
+    {
+
+    }
+}
---
+++ Prog3.RestoDotNet.Model/Entities/Meal.cs
@@ -0,0 +1,13 @@
+﻿using Pandora.NetStandard.Core.Interfaces;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Prog3.RestoDotNet.Model.Entities
+{
+    [Table("Meals", Schema = "Resto")]
+    public class Meal : IEntity
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
