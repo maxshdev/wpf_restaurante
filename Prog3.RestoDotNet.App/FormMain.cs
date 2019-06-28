bdb6f049f4d670778c2f12f4d65e2ae90a06ed64
maxshtefec <max.shtefec@gmail.com>
2019-06-28 14:35:22 -0300

    fix 2

--- Prog3.RestoDotNet.App/FormMain.cs
+++ Prog3.RestoDotNet.App/FormMain.cs
@@ -73,13 +73,14 @@ namespace Prog3.RestoDotNet.App
 
         private void VerToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            Control temp = new Control();
             ToolStripItem item = (sender as ToolStripItem);
             if (item != null)
             {
                 ContextMenuStrip owner = item.Owner as ContextMenuStrip;
                 if (owner != null)
                 {
-                    Control temp = owner.SourceControl;
+                    temp = owner.SourceControl;
                 }
             }
 
